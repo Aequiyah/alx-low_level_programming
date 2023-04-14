b10b774a65084baea290408aab9a92dd3a98f7e0
Aequiyah <akuakwarteng3@gmail.com>
2023-04-13 22:22:02 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,52 @@
 #include "main.h"
 
+#define CHAR_BITS 8 /*size of char*/
+#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
+
 /**
- * get_bit - Returns the value of a bit at a given index.
- * @n: The number whose bit value is to be retrieved.
- * @index: The index of the bit to be retrieved, starting from 0 (LSB).
+ * print_bin - prints binary rep of @n
  *
- * Return: The value of the bit at the given index (0 or 1), or -1 if an error occurred.
- */
-int get_bit(unsigned long int n, unsigned int index)
+ * @number: decimal value
+ *
+ * Return: nothing
+*/
+void print_bin(unsigned long int number)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);  /* Return -1 if index is out of range */
+	if (number >> 1)
+		print_bin(number >> 1);
 
-	unsigned long int mask = 1UL << index; /* Create a mask with 1 at the given index */
-	int bit_value = (n & mask) ? 1 : 0; /* Use bitwise AND to extract the bit value */
-
-	return (bit_value);
+	putc((number & 1) ? '1' : '0', stdout);
 }
 
+
+/**
+ * get_bit - gets a bit at a given index
+ *
+ * @n: decimal value
+ * @index: integer value of bit position
+ *        to get from @n
+ *
+ * Return: the value of the bit or -1 if an
+ *          error occured
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	/*if index is greater than size of n in binary coded decimal*/
+	if (index > INT_BITS)
+		return (-1);
+
+	#ifdef DEBUG
+	printf("\n[%ld >> %d] is %ld.\n", n, index, (n >> index));
+	printf("%ld in binary: ", n);
+	print_bin(n);
+	printf("\n[%ld >> %d] in binary: ", n, index);
+	print_bin(n >> index);
+	printf(" <- last bit is bit at index %d\n\n", index);
+	#endif
+
+	bit = ((n >> index) & 1); /*shift n right by index and with 1 to find bit*/
+
+	return (bit);
+}
