c0b559e8f678f750e006eaac8052c94039e5719d
Aequiyah <akuakwarteng3@gmail.com>
2023-04-26 02:43:39 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,15 @@
-#include "main.h"
-
 /**
- * get_endianness - Function to check the endianness of the system
+ * get_endianness - checks the endianness of the system
  *
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	unsigned int num = 1;
-	char *ptr = (char *)&num;
+	unsigned int x = 1;
+	char *c = (char *)&x;
 
-	if (*ptr == 1)
-		return 1; // Little Endian
-	else
-		return 0; // Big Endian
-}
-
+	if (*c)
+	    return 1;
+        else 
+	    return 0;
+}	
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,52 +1,15 @@
-#include "main.h"
-
-#define CHAR_BITS 8 /*size of char*/
-#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
-
 /**
- * print_bin - prints binary rep of @n
+ * get_bit - Returns the value of a bit at a given index
+ * @n: The number to extract the bit from
+ * @index: The index of the bit to retrieve, starting from 0.
  *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
+ * Return: The value of the bit at the given index, or -1 if an error occurred.
+ * */
+int get _bit(unsigned long int n, unsigned int index)
 {
-	if (number >> 1)
-		print_bin(number >> 1);
-
-	putc((number & 1) ? '1' : '0', stdout);
-}
+    if (index >= sizeof(unsigned long int) * 8)
+       return (-1); // index out of range
 
 
-/**
- * get_bit - gets a bit at a given index
- *
- * @n: decimal value
- * @index: integer value of bit position
- *        to get from @n
- *
- * Return: the value of the bit or -1 if an
- *          error occured
-*/
-int get_bit(unsigned long int n, unsigned int index)
-{
-	int bit;
-
-	/*if index is greater than size of n in binary coded decimal*/
-	if (index > INT_BITS)
-		return (-1);
-
-	#ifdef DEBUG
-	printf("\n[%ld >> %d] is %ld.\n", n, index, (n >> index));
-	printf("%ld in binary: ", n);
-	print_bin(n);
-	printf("\n[%ld >> %d] in binary: ", n, index);
-	print_bin(n >> index);
-	printf(" <- last bit is bit at index %d\n\n", index);
-	#endif
-
-	bit = ((n >> index) & 1); /*shift n right by index and with 1 to find bit*/
-
-	return (bit);
-}
+    return ((n >> index)&1);
+}   
